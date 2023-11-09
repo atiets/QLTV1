c272a38803589c5c234ad1b6dea046edd016b9a9
atiets <132215716+atiets@users.noreply.github.com>
2023-11-09 20:22:29 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace QLTV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label_Home = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_ThuThu = new System.Windows.Forms.Button();
+            this.btn_MuonSach = new System.Windows.Forms.Button();
+            this.btn_TheThuVien = new System.Windows.Forms.Button();
+            this.btn_DocGia = new System.Windows.Forms.Button();
+            this.btn_BienLai = new System.Windows.Forms.Button();
+            this.btn_TacGia = new System.Windows.Forms.Button();
+            this.btn_QDP = new System.Windows.Forms.Button();
+            this.btn_TheLoai = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.splitContainer1.Panel1.Controls.Add(this.label_Home);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_ThuThu);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_MuonSach);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_TheThuVien);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_DocGia);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_BienLai);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_TacGia);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_QDP);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_TheLoai);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(1422, 635);
+            this.splitContainer1.SplitterDistance = 187;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label_Home
+            // 
+            this.label_Home.AutoSize = true;
+            this.label_Home.Font = new System.Drawing.Font("Segoe Script", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Home.Location = new System.Drawing.Point(28, 531);
+            this.label_Home.Name = "label_Home";
+            this.label_Home.Size = new System.Drawing.Size(126, 57);
+            this.label_Home.TabIndex = 5;
+            this.label_Home.Text = "Home";
+            this.label_Home.Click += new System.EventHandler(this.label_Home_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(15, 464);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 42);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Đầu Sách";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(14, 425);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 42);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Cuốn Sách";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_ThuThu
+            // 
+            this.btn_ThuThu.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btn_ThuThu.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ThuThu.Location = new System.Drawing.Point(14, 386);
+            this.btn_ThuThu.Name = "btn_ThuThu";
+            this.btn_ThuThu.Size = new System.Drawing.Size(156, 42);
+            this.btn_ThuThu.TabIndex = 11;
+            this.btn_ThuThu.Text = "Thủ Thư";
+            this.btn_ThuThu.UseVisualStyleBackColor = false;
+            // 
+            // btn_MuonSach
+            // 
+            this.btn_MuonSach.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btn_MuonSach.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_MuonSach.Location = new System.Drawing.Point(14, 308);
+            this.btn_MuonSach.Name = "btn_MuonSach";
+            this.btn_MuonSach.Size = new System.Drawing.Size(156, 42);
+            this.btn_MuonSach.TabIndex = 12;
+            this.btn_MuonSach.Text = "Mượn Sách";
+            this.btn_MuonSach.UseVisualStyleBackColor = false;
+            // 
+            // btn_TheThuVien
+            // 
+            this.btn_TheThuVien.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btn_TheThuVien.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TheThuVien.Location = new System.Drawing.Point(13, 230);
+            this.btn_TheThuVien.Name = "btn_TheThuVien";
+            this.btn_TheThuVien.Size = new System.Drawing.Size(156, 42);
+            this.btn_TheThuVien.TabIndex = 13;
+            this.btn_TheThuVien.Text = "Thẻ Thư Viện";
+            this.btn_TheThuVien.UseVisualStyleBackColor = false;
+            // 
+            // btn_DocGia
+            // 
+            this.btn_DocGia.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btn_DocGia.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DocGia.Location = new System.Drawing.Point(13, 191);
+            this.btn_DocGia.Name = "btn_DocGia";
+            this.btn_DocGia.Size = new System.Drawing.Size(156, 42);
+            this.btn_DocGia.TabIndex = 8;
+            this.btn_DocGia.Text = "Độc Giả";
+            this.btn_DocGia.UseVisualStyleBackColor = false;
+            this.btn_DocGia.Click += new System.EventHandler(this.btn_DocGia_Click);
+            // 
+            // btn_BienLai
+            // 
+            this.btn_BienLai.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btn_BienLai.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_BienLai.Location = new System.Drawing.Point(13, 347);
+            this.btn_BienLai.Name = "btn_BienLai";
+            this.btn_BienLai.Size = new System.Drawing.Size(156, 42);
+            this.btn_BienLai.TabIndex = 9;
+            this.btn_BienLai.Text = "Biên Lai";
+            this.btn_BienLai.UseVisualStyleBackColor = false;
+            // 
+            // btn_TacGia
+            // 
+            this.btn_TacGia.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btn_TacGia.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TacGia.Location = new System.Drawing.Point(13, 152);
+            this.btn_TacGia.Name = "btn_TacGia";
+            this.btn_TacGia.Size = new System.Drawing.Size(156, 42);
+            this.btn_TacGia.TabIndex = 7;
+            this.btn_TacGia.Text = "Tác Giả";
+            this.btn_TacGia.UseVisualStyleBackColor = false;
+            // 
+            // btn_QDP
+            // 
+            this.btn_QDP.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btn_QDP.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_QDP.Location = new System.Drawing.Point(13, 269);
+            this.btn_QDP.Name = "btn_QDP";
+            this.btn_QDP.Size = new System.Drawing.Size(156, 42);
+            this.btn_QDP.TabIndex = 10;
+            this.btn_QDP.Text = "Quy Định Phạt";
+            this.btn_QDP.UseVisualStyleBackColor = false;
+            // 
+            // btn_TheLoai
+            // 
+            this.btn_TheLoai.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btn_TheLoai.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TheLoai.Location = new System.Drawing.Point(12, 113);
+            this.btn_TheLoai.Name = "btn_TheLoai";
+            this.btn_TheLoai.Size = new System.Drawing.Size(156, 42);
+            this.btn_TheLoai.TabIndex = 6;
+            this.btn_TheLoai.Text = "Thể loại";
+            this.btn_TheLoai.UseVisualStyleBackColor = false;
+            this.btn_TheLoai.Click += new System.EventHandler(this.btn_TheLoai_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::QLTV1.Properties.Resources.logoo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1422, 635);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label_Home;
+        private System.Windows.Forms.Button btn_ThuThu;
+        private System.Windows.Forms.Button btn_MuonSach;
+        private System.Windows.Forms.Button btn_TheThuVien;
+        private System.Windows.Forms.Button btn_DocGia;
+        private System.Windows.Forms.Button btn_BienLai;
+        private System.Windows.Forms.Button btn_TacGia;
+        private System.Windows.Forms.Button btn_QDP;
+        private System.Windows.Forms.Button btn_TheLoai;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using QLTV1.Models;
+using QLTV1.UC;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLTV1
+{
+    public partial class Form1 : Form
+    {
+        private Button curbtn;
+        private TheLoaiUC theloaiuc = new TheLoaiUC();
+        private DocGiaUC docgiauc = new DocGiaUC();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_TheLoai_Click(object sender, EventArgs e)
+        {
+            if(curbtn != null)
+            {
+                curbtn = btn_TheLoai;
+            }
+            if (!splitContainer1.Panel2.Controls.Contains(theloaiuc))
+            {
+                splitContainer1.Panel2.Controls.Clear();
+                theloaiuc.Dock = DockStyle.Fill;
+                splitContainer1.Panel2.Controls.Add(theloaiuc);
+            }
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                if (curbtn != null)
+                {
+                    curbtn = btn_TheLoai;
+                }
+                if (!splitContainer1.Panel2.Controls.Contains(theloaiuc))
+                {
+                    splitContainer1.Panel2.Controls.Clear();
+                    theloaiuc.Dock = DockStyle.Fill;
+                    splitContainer1.Panel2.Controls.Add(theloaiuc);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_DocGia_Click(object sender, EventArgs e)
+        {
+            if (curbtn != null)
+            {
+                curbtn = btn_TheLoai;
+            }
+            if (!splitContainer1.Panel2.Controls.Contains(docgiauc))
+            {
+                splitContainer1.Panel2.Controls.Clear();
+                docgiauc.Dock = DockStyle.Fill;
+                splitContainer1.Panel2.Controls.Add(docgiauc);
+            }
+        }
+
+        private void label_Home_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
